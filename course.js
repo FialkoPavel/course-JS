0b233a62ac97b906bc06ba0a26f43ef51a4ccf5b
Pavel <fialkopavel@gmail.com>
2019-11-04 16:40:26 +0200

    git commit -a -m"course JS"

--- course.js
+++ course.js
@@ -1,16 +1,21 @@
-let money = prompt('Ваш бюджет на месяц?', 0);
-let time = prompt('Введите дату в формате YYYY-MM-DD', 0);
-let article = prompt('Введите обязательную статью расходов в этом месяце', 0);
-let cost = prompt('Во сколько обойдется?', 0);
-let day;
+let money = prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
 let appData = {
 	budget: money,
 	timeData: time,
-	expenses: {article, cost} ,
+	expenses: {},
+	optionalExpenses: {} ,
 	income: [],
 	savings: false
 }
-day = +money/30;
 
-console.log(appData);
-alert(day);
+let a1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let a2 = prompt('Во сколько обойдется?', 0);
+let a3 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let a4 = prompt('Во сколько обойдется?', 0);
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget/30);
